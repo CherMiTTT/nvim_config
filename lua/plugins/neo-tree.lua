d281319e34dd00deb8f97e20e9583248f9ec1ba2
CherMiTTT <chermi20000@yandex.ru>
2023-02-07 22:08:48 +0300

    Initial config commit

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "left",
+        width = 20,
+        auto_expand_width = true,
+      },
+      source_selector = {
+        winbar = true,
+        statusline = false,
+      },
+    },
+  },
+}
